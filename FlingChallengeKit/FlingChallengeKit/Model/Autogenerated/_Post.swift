c6219dae3eb9f64e5636e4dd9a48bf91e10a8350
Alexander Kolov <me@alexkolov.com>
2016-05-08 18:21:22 +0200

    Add initial model and generate sources

---
+++ FlingChallengeKit/FlingChallengeKit/Model/Autogenerated/_Post.swift
@@ -0,0 +1,58 @@
+// DO NOT EDIT. This file is machine-generated and constantly overwritten.
+// Make changes to Post.swift instead.
+
+import Foundation
+import CoreData
+
+public enum PostAttributes: String {
+    case identifier = "identifier"
+    case imageID = "imageID"
+    case title = "title"
+    case userID = "userID"
+    case userName = "userName"
+}
+
+public class _Post: NSManagedObject {
+
+    // MARK: - Class methods
+
+    public class func entityName () -> String {
+        return "Post"
+    }
+
+    public class func entity(managedObjectContext: NSManagedObjectContext) -> NSEntityDescription? {
+        return NSEntityDescription.entityForName(self.entityName(), inManagedObjectContext: managedObjectContext)
+    }
+
+    // MARK: - Life cycle methods
+
+    public override init(entity: NSEntityDescription, insertIntoManagedObjectContext context: NSManagedObjectContext?) {
+        super.init(entity: entity, insertIntoManagedObjectContext: context)
+    }
+
+    public convenience init?(managedObjectContext: NSManagedObjectContext) {
+        guard let entity = _Post.entity(managedObjectContext) else { return nil }
+        self.init(entity: entity, insertIntoManagedObjectContext: managedObjectContext)
+    }
+
+    // MARK: - Properties
+
+    @NSManaged public
+    var identifier: NSNumber?
+
+    @NSManaged public
+    var imageID: NSNumber?
+
+    @NSManaged public
+    var title: String?
+
+    @NSManaged public
+    var userID: NSNumber?
+
+    @NSManaged public
+    var userName: String
+
+    // MARK: - Relationships
+
+}
+
---
+++ FlingChallengeKit/FlingChallengeKit/Model/Post.swift
@@ -0,0 +1,14 @@
+//
+//  Post.swift
+//  FlingChallengeKit
+//
+//  Created by Alexander Kolov on 5/8/16.
+//  Copyright © 2016 Alexander Kolov. All rights reserved.
+//
+
+import Foundation
+
+@objc(Post)
+public class Post: _Post {
+	// Custom logic goes here.
+}
